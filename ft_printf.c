099209c3c7374594fb78396d7b1fb1e83d4ab943
Coindreau <coindreau@MacBook-Air-de-Coindreau.local>
2022-11-18 15:32:03 +0100

    en cours

--- ft_printf.c
+++ ft_printf.c
@@ -14,72 +14,74 @@
 #include <stdio.h>
 #include "libft/libft.h"
 #include "libftprintf.h"
+void	ft_putstr_cnt(char *str, int *c_count);
+void	ft_print_format(const char c_format, va_list *args, int *ptr_c_count);
+void	ft_putchar_cnt(char c, int *c_count);
 
 
-int ft_check_type(char c)
+void ft_print_format(const char c_format, va_list *args, int *ptr_c_count)
 {
-	int i;
-
-	i = 0;
-	while (i < 9)
+	if (c_format == 'c')
+		ft_putchar_cnt(va_arg(*args, int), ptr_c_count);
+	else if (c_format == 's')
 	{
-		if (c == LST_TYPES[i])
-			return (i);
-		i++;		
+		ft_putstr_cnt(va_arg(*args, char*), ptr_c_count);
 	}
-	return (-1);
+	
 }
 
+void	ft_putchar_cnt(char c, int *c_count)
+{
 
+	write(1, &c, 1);
+
+	*c_count += 1;
+}
+
+void	ft_putstr_cnt(char *str, int *c_count)
+{
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+		*c_count += 1;
+	}
+}
 
 int ft_printf(const char *str, ...)
 {
 
-	size_t	size_str;
 	size_t	i;
-	int		i_type;
 	va_list	args;
+	int		c_count;
 
-	size_str = ft_strlen(str);
 
+	c_count = 0;
 	va_start(args, str);
-
-
-
 	i = 0;
-	while (i < size_str)
+	while (str[i] != '\0')
 	{
-		
-		if (str[i] != '%')
+		if (str[i] == '%')
 		{
-			if (write(1,&str[i],1) == -1)
-				return(-1);
+			ft_print_format(str[i + 1], &args, &c_count);
+			i++;
 		}
 		else
 		{
-			i_type = ft_check_type(str[i + 1]);
-
+			write(1,&str[i],1);
+			c_count++;
 		}
-		
 		i++;
 	}
 
-	int val = va_arg(args, int);
-
-	printf("val = %d\n", val);
-	
-	val = va_arg(args, int);
-
-	printf("val = %d\n", val);
-
 	va_end(args);
 
-	return 0;
+	return c_count;
 }
 
 int main()
 {
-	ft_printf("Ce %d caractere a parser\n", 12, 34, 56);
+	ft_printf("A%c B%s C%c\n", 'a', "assin", 'c');
 
 
 	return 0;
